1b9d45a01e2cae2075bfed75aab0e8760e9835d8
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2015-11-19 10:28:48 +0500

    Добавил шедуллер

--- sex_lt_programming.py
+++ sex_lt_programming.py
@@ -1,16 +1,26 @@
 # coding: utf-8
+
+from apscheduler.schedulers.blocking import BlockingScheduler
 from bot import Zaebot
+
 bot = Zaebot()
+sched = BlockingScheduler()
 
 phrase = u'ебаться надо'
+replacement = u'программировать надо'
 phrase_correct = lambda tweet: phrase in tweet
 not_hashtag_or_reply = lambda tweet: u'@' not in tweet and u'#' not in tweet
 tweet_to_text = lambda tweet: tweet.text.lower()
 
-tweets = bot.reader.api.search(phrase, count=200)
-tweets_text = map(tweet_to_text, tweets)
-tweets_text = filter(not_hashtag_or_reply, filter(phrase_correct, tweets_text))
+@sched.scheduled_job('interval', minutes=60)
+def do_tweets():
+    tweets = bot.reader.api.search(phrase, count=200)
+    tweets_text = map(tweet_to_text, tweets)
+    tweets_text = filter(phrase_correct, tweets_text)
+    tweets_text = filter(not_hashtag_or_reply, tweets_text)
 
-for tweet in tweets_text:
-    tweet_text = tweet.replace(u'ебаться', u'программировать').encode('utf-8')
-    bot.writer.tweet(tweet_text)
+    for tweet in tweets_text:
+        tweet_text = tweet.replace(phrase, replacement).encode('utf-8')
+        bot.writer.tweet(tweet_text)
+
+sched.start()
