1f24786f3d7db8259b421456b1793ac388cbea32
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2015-11-19 10:20:45 +0500

    Добавил твитилку

--- sex_lt_programming.py
+++ sex_lt_programming.py
@@ -1,15 +1,16 @@
 # coding: utf-8
 from bot import Zaebot
-x = Zaebot()
+bot = Zaebot()
 
 phrase = u'ебаться надо'
 phrase_correct = lambda tweet: phrase in tweet
 not_hashtag_or_reply = lambda tweet: u'@' not in tweet and u'#' not in tweet
 tweet_to_text = lambda tweet: tweet.text.lower()
 
-tweets = x.reader.api.search(phrase, count=200)
+tweets = bot.reader.api.search(phrase, count=200)
 tweets_text = map(tweet_to_text, tweets)
 tweets_text = filter(not_hashtag_or_reply, filter(phrase_correct, tweets_text))
 
-for i in tweets_text:
-    print i.replace(u'ебаться', u'программировать').encode('utf-8')
+for tweet in tweets_text:
+    tweet_text = tweet.replace(u'ебаться', u'программировать').encode('utf-8')
+    bot.writer.tweet(tweet_text)
