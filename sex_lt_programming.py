9f12e0116ec2319bc658809080f00b2e22a99270
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2015-11-24 13:36:29 +0500

    Рефакторинг

--- sex_lt_programming.py
+++ sex_lt_programming.py
@@ -2,42 +2,70 @@
 """ раз в час пытаемся запостить цитатку про то как надо программировать """
 
 from apscheduler.schedulers.blocking import BlockingScheduler
-from bot import Zaebot
+from tweet import Twibot
 from hashlib import md5
 
-bot = Zaebot()
+bot = Twibot()
 sched = BlockingScheduler()
-
-phrase = u'ебаться надо'
-replacement = u'программировать надо'
-phrase_correct = lambda tweet: phrase in tweet
-not_hashtag_or_reply = lambda tweet: u'@' not in tweet and u'#' not in tweet
-tweet_to_text = lambda tweet: tweet.text.lower()
 hasher = md5()
 
+not_hashtag_or_reply = lambda tweet: u'@' not in tweet and u'#' not in tweet
+tweet_to_text = lambda tweet: tweet.text.lower()
+
+replacements = {
+    u'ебаться надо': {
+        u'ебаться': u'программировать',
+    },
+    u'в сексе главное': {
+        u'сексе':   u'программировании',
+    },
+    u'трахаться надо': {
+        u'трахаться': u'программировать',
+    },
+    u'заниматься сексом надо': {
+        u'заниматься сексом': u'программировать',
+        u'сексом заниматься': u'программировать',
+    },
+    u'ебаться в жопу': {
+        u'ебаться': u'пушить',
+        u'в жопу': u'с форсом',
+    },
+}
+
+
 def get_hashes():
+    """ хэшики последних 200 своих твитов """
     hashes = []
-    me = bot.writer.api.me()
+    me = bot.api.me()
     timeline = me.timeline(count=200)
     for tweet in [t.text for t in timeline]:
         hasher.update(tweet.encode('utf-8'))
         hashes.append(hasher.hexdigest())
     return list(set(hashes))
 
+
+def process_tweet(tweet, replaces, hashes):
+    """ обрабатываем полученные твиты и постим новые """
+    for word, replace in replaces.items():
+        tweet = tweet.replace(word, replace)
+    tweet = tweet.encode('utf-8')
+    hasher.update(tweet)
+    if hasher.hexdigest() in hashes:
+        return
+    bot.tweet(tweet)
+
+
 @sched.scheduled_job('interval', minutes=60)
 def do_tweets():
+    """ тянем нужные твиты и скармливаем постилке """
     hashes = get_hashes()
-    tweets = bot.reader.api.search(phrase, count=200)
-    tweets_text = map(tweet_to_text, tweets)
-    tweets_text = filter(phrase_correct, tweets_text)
-    tweets_text = filter(not_hashtag_or_reply, tweets_text)
+    for phrase, replaces in replacements.items():
+        print phrase.encode('utf-8')
+        tweets = bot.api.search(phrase, count=10)
+        tweets_text = map(tweet_to_text, tweets)
+        tweets_text = filter(not_hashtag_or_reply, tweets_text)
+        for tweet in tweets_text:
+            process_tweet(tweet, replaces, hashes)
 
-    for tweet in tweets_text:
-        tweet_text = tweet.replace(phrase, replacement).encode('utf-8')
-        print 'checking', tweet_text
-        hasher.update(tweet_text)
-        if hasher.hexdigest() not in hashes:
-            print 'not posted before'
-            bot.writer.tweet(tweet_text)
 
 sched.start()
