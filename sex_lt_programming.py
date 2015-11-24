aa127223d1f5ac830ff897351a5174c04aeeeaff
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2015-11-24 14:25:58 +0500

    logging

--- sex_lt_programming.py
+++ sex_lt_programming.py
@@ -90,6 +90,7 @@ def do_tweets():
     """ тянем нужные твиты и скармливаем постилке """
     hashes = get_hashes()
     for phrase, replaces in replacements.items():
+        print '# search:', phrase.encode('utf-8')
         tweets = bot.api.search(phrase, count=20, result_type='recent')
         tweets_text = map(tweet_to_text, tweets)
         tweets_text = filter(not_hashtag_or_reply, tweets_text)
