73a949e3925e17d188641a0a1ffa67bdb78d1a5c
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2015-12-22 12:07:06 +0500

    Хэши стали лучше, меньше encode, записал блэклист и замену короче, увеличил sleep между твитами

--- sex_lt_programming.py
+++ sex_lt_programming.py
@@ -6,6 +6,7 @@ from tweet import Twibot
 from hashlib import md5
 from time import sleep
 import sys
+import re
 
 bot = Twibot()
 sched = BlockingScheduler()
@@ -15,33 +16,17 @@ tweet_to_text = lambda tweet: tweet.text.lower()
 hashes = []
 
 blacklist = (
-    u'выпущено 24 кролика',
-    u'учёные выяснили',
-    u'ученые выяснили',
-    u'учёные узнали',
-    u'ученые узнали',
-    u'как часто',
-    u'психологи узнали',
-    u'психологи выяснили',
-    u'http://',
-    u'https://',
-    u'.com',
-    u't.co',
+    u'выпущено 24 кролика', u'учёные выяснили', u'ученые выяснили',
+    u'учёные узнали', u'ученые узнали', u'как часто',
+    u'психологи узнали', u'психологи выяснили', u'http://',
+    u'https://', u'.com', u't.co',
 )
 
 replacements = {
-    u'ебутся': {
-        u'ебутся': u'программируют',
-    },
-    u'ебаться': {
-        u'ебаться': u'программировать',
-    },
-    u'трахаться': {
-        u'трахаться': u'программировать',
-    },
-    u'в сексе главное': {
-        u'сексе':   u'программировании',
-    },
+    u'ебутся': {u'ебутся': u'программируют', },
+    u'ебаться': {u'ебаться': u'программировать', },
+    u'трахаться': {u'трахаться': u'программировать', },
+    u'в сексе главное': {u'сексе':   u'программировании', },
     u'заниматься сексом': {
         u'заниматься сексом': u'программировать',
         u'сексом заниматься': u'программировать',
@@ -52,39 +37,43 @@ replacements = {
     },
 }
 
+
 def not_blacklisted(tweet):
+    """ некоторые твиты очень часто проскальзывают из-за модификаций """
     for phrase in blacklist:
         if phrase in tweet:
             return False
     return True
 
+
+def get_hash(tweet):
+    """ учитываем что люди тупые и часто немного меняют спизженный твит """
+    return md5(re.sub('[ .,!?-]', '', tweet)).hexdigest()
+
+
 def get_hashes():
     """ хэшики последних 200 своих твитов """
-    hashes = []
-    me = bot.api.me()
-    timeline = me.timeline(count=200)
-    for tweet in [t.text for t in timeline]:
-        hashes.append(md5(tweet.encode('utf-8')).hexdigest())
-    return list(set(hashes))
+    hashlist = []
+    for tweet in [t.text for t in bot.api.me().timeline(count=200)]:
+        hashlist.append(get_hash(tweet))
+    return list(set(hashlist))
 
 
-def process_tweet(tweet, replaces, hashes):
+def process_tweet(tweet, replaces, hashlist):
     """ обрабатываем полученные твиты и постим новые """
-    tweet_original = tweet
+    replaced_tweet = tweet
     for word, replace in replaces.items():
-        tweet = tweet.replace(word, replace)
-    if tweet_original == tweet:
+        replaced_tweet = replaced_tweet.replace(word, replace)
+    if replaced_tweet == tweet:
         return
-    if len(tweet) > 140:
-        if isinstance(tweet, unicode):
-            tweet = tweet.encode('utf-8')
+    if isinstance(replaced_tweet, unicode):
+        replaced_tweet = replaced_tweet.encode('utf-8')
+    if len(replaced_tweet) > 140:
         print 'too long :( :', tweet
         return
-    tweet = tweet.encode('utf-8')
-    tweet_hash = md5(tweet).hexdigest()
-    if tweet_hash in hashes:
+    if get_hash(replaced_tweet) in hashlist:
         return
-    bot.tweet(tweet)
+    bot.tweet(replaced_tweet)
 
 
 @sched.scheduled_job('interval', minutes=15)
@@ -100,7 +89,7 @@ def do_tweets():
         tweets_text = filter(not_blacklisted, tweets_text)
         for tweet in tweets_text:
             process_tweet(tweet, replaces, hashes)
-            sleep(2)
+            sleep(10)
 
 
 if __name__ == '__main__':
