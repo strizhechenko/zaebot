27f01e64cf8a7086ef793dccab70a7c1857297a9
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2015-12-30 15:41:04 +0500

    Лишнее логирование исключено

--- bot.py
+++ bot.py
@@ -56,10 +56,8 @@ def get_hashes(tweets=None):
     if not (tweets and isinstance(tweets, list)):
         return []
     if isinstance(tweets[0], tweepy.models.Status):
-        print "get_hashes: Status"
         hashlist.extend([get_hash(t.text.encode('utf-8')) for t in tweets])
-    if isinstance(tweets[0], unicode):
-        print "get_hashes: unicode"
+    elif isinstance(tweets[0], unicode):
         hashlist.extend([get_hash(t.encode('utf-8')) for t in tweets])
     return list(set(hashlist))
 
