89a7d356e72b0ef725334f2810e1b6e039c50884
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-01-21 23:35:46 +0500

    Параметры связанные с числом твитов тоже занесены в конфиг

--- bot.py
+++ bot.py
@@ -13,15 +13,17 @@ bot = Twibot()
 reader = Twibot(username=os.environ.get('reader_name'))
 morphy = Morpher()
 timeout = int(os.environ.get('timeout', 30))
-
+template = unicode(os.environ.get('template', u''), 'utf-8')
+tweet_grab = int(os.environ.get('tweet_grab', 3))
+tweets_per_tick = int(os.environ.get('tweets_per_tick', 2))
 
 @sched.scheduled_job('interval', minutes=timeout)
 def do_tweets():
     print 'New tick'
-    tweets = reader.api.home_timeline(count=3)
+    tweets = reader.api.home_timeline(count=tweet_grab)
     string = " ".join([tweet.text for tweet in tweets])
-    words = morphy.process_to_words(string, count=2)
-    posts = [u"%s – это когда тебя в жопу ебут." % (word) for word in words]
+    words = morphy.process_to_words(string, count=tweets_per_tick)
+    posts = [template % (word) for word in words]
     bot.tweet_multiple(posts, logging=True)
     print 'Wait for', timeout, 'minutes'
 
